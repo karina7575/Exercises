6d4211b1d3ed456ec86c36402ad43342dfa33ec2
Karina <karinakios@mail.ru>
2023-04-25 23:13:58 +0300

    25.04

--- SignIn.java
+++ SignIn.java
@@ -2,16 +2,22 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.Scanner;
-public class One
+
+public class SignIn
 {
     public static void main( String[] args ) {
-        JFrame frame = new JFrame( "HelloJava2" );
+        JFrame frame = new JFrame( "Sign in" );
         Scanner in = new Scanner(System.in);
         String mes = in.nextLine();
         frame.add( new HelloComponent2( mes ) );
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         frame.setSize( 300, 300 );
         frame.setVisible( true );
+        JButton button = new JButton("Enter");
+        //button.setSize(10, 10);
+        frame.add(BorderLayout.PAGE_END, button); // Adds Button to content pane of frame
+        button.setSize(10, 10);
+        frame.setVisible(true);
     }
 }
 class HelloComponent2 extends JComponent
