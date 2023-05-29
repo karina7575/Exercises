c9b40db2de3592946202d4c492e88cf1e7b04505
Karina <karinakios@mail.ru>
2023-05-29 22:43:04 +0300

    29.05

---
+++ Collection/LinkedHashSetExample.java
@@ -0,0 +1,22 @@
+package Collection;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetExample {
+    public static void main(String[] args) {
+        LinkedHashSet<Integer> lhs = new LinkedHashSet<>();
+        lhs.add(5);
+        lhs.add(3);
+        lhs.add(1);
+        lhs.add(9);
+        lhs.add(10);
+        System.out.println(lhs);
+        lhs.remove(9);
+        System.out.println(lhs);
+        System.out.println(lhs.contains(9));
+        System.out.println(lhs.contains(10));
+
+
+    }
+}
+
--- Collection/TreeSetExample.java
+++ Collection/TreeSetExample.java
@@ -1,23 +1,24 @@
 package Collection;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class TreeSetExample
 {
     public static void main(String[] args) {
-        Set<Integer> treeSet = new TreeSet<>();
-        treeSet.add(5);
-        treeSet.add(8);
-        treeSet.add(2);
-        treeSet.add(1);
-        treeSet.add(10);
-        treeSet.remove(2);
-        System.out.println(treeSet.contains(1));
-        System.out.println(treeSet);
+//        Set<Integer> treeSet = new TreeSet<>();
+//        treeSet.add(5);
+//        treeSet.add(8);
+//        treeSet.add(2);
+//        treeSet.add(1);
+//        treeSet.add(10);
+//        treeSet.remove(2);
+//        System.out.println(treeSet.contains(1));
+//        System.out.println(treeSet);
 
-        Set<Studentt> treeSet1 = new TreeSet<>();
+        TreeSet<Studentt> treeSet1 = new TreeSet<>();
         Studentt st1 = new Studentt("Zaur", 5);
         Studentt st2 = new Studentt("Nelya", 1);
         Studentt st3 = new Studentt("Elena", 2);
@@ -28,8 +29,16 @@ public class TreeSetExample
         treeSet1.add(st3);
         treeSet1.add(st4);
         treeSet1.add(st5);
+        Studentt st6 = new Studentt("Oleg", 2);
         System.out.println(treeSet1);
-
+        System.out.println(treeSet1.first());                    //самый маленький ключ
+        System.out.println(treeSet1.last());                     //самый большой ключ
+        System.out.println(treeSet1.headSet(st6));              //выводит студентов, которые на курсах ниже,чем st6
+        System.out.println(treeSet1.headSet(st6));              //выводит студентов, которые на курсах выше или на том же,что и st6
+        Studentt st7 = new Studentt("Ivan", 4);
+        System.out.println(treeSet1.subSet(st6, st7));          //выводит студентов, на курсе меньше, чем st6 и больше, чем st7
+        System.out.println(st3.equals(st6));
+        System.out.println(st3.hashCode() == st6.hashCode());
 
     }
 }
@@ -54,5 +63,18 @@ class Studentt implements Comparable<Studentt>
                 "name='" + name + '\'' +
                 ", course=" + course +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Studentt studentt = (Studentt) o;
+        return course == studentt.course;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(course);
     }
 }
